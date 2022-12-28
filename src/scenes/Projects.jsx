720f8d1c0affee8609a835f271135a4c9cf3b012
Mayureshju <juvekarmayuresh37@gmail.com>
2022-12-28 18:55:46 +0530

    final commit

---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,85 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+        My project
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-40 pb-40">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+        Here are a few projects on which I have worked.
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      
+    </section>
+  );
+};
+
+
+
+
+// const images = [
+//   { url: "assets/project-2.jpg" },
+//   { url: "assets/project-3.jpg" },
+//   { url: "assets/project-4.jpg" },
+//   { url: "assets/project-5.jpg" },
+//   { url: "assets/project-6.jpg" },
+
+// ];
+
+
+
+export default Projects;
